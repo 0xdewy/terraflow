531f8fc11f2c37810325e81285fcd84d56836016
kyle <15720036+kyledewy@users.noreply.github.com>
2023-06-04 16:30:42 -0700

    Generate oceans and mountains. Start on general map generation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+
 use bevy_basic_camera::{CameraController, CameraControllerPlugin};
 use bevy_mod_picking::prelude::*;
 
@@ -6,7 +7,7 @@ use hexx::shapes;
 use hexx::*;
 
 use rand::prelude::SliceRandom;
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 fn main() {
     App::new()
@@ -19,6 +20,7 @@ fn main() {
         .add_plugin(CameraControllerPlugin)
         .add_startup_system(setup_camera)
         .add_startup_system(setup_grid)
+        .add_system(bevy::window::close_on_esc)
         .run();
 }
 
@@ -28,33 +30,243 @@ const ELEVATION: f32 = 10.0;
 // Vulcanism -> Spawns mountains
 const VOLCANISM: f32 = 3.0;
 // How many rings to traverse from the volcano to spawn mountains
-const MOUNTAIN_SPREAD: u32 = MAP_RADIUS * 60 / 100;
+const MOUNTAIN_SPREAD: u32 = MAP_RADIUS * 90 / 100;
 // How much to increase the elevation by
 const ELEVATION_INCREMENT: f32 = 0.5;
 // Elevation where sea level is
-const SEA_LEVEL: f32 = 5.0;
+const SEA_LEVEL: f32 = 3.0;
 // Planet Age
-const PLANET_AGE: u128 = 0;
+const _PLANET_AGE: u128 = 0;
 // Base Temperature
-const BASE_TEMPERATURE: f32 = 20.0;
+const _BASE_TEMPERATURE: f32 = 20.0;
 
 ////////////////////// GRID //////////////////////
 
 /// World size of the hexagons (outer radius)
 const HEX_SIZE: Vec2 = Vec2::splat(2.0);
 /// Map radius
-const MAP_RADIUS: u32 = 20;
+const MAP_RADIUS: u32 = 40;
 
 // TODO: store tile type + innate attributes instead of the material
 #[derive(Resource)]
 struct Map {
-    entities: HashMap<Hex, (Entity, Handle<StandardMaterial>)>,
+    entities: HashMap<Hex, (Entity)>,
 }
 
-// To avoid having a fixed probability, we can make it a function of distance
+/// Hex grid setup
+fn setup_grid(asset_server: Res<AssetServer>, mut commands: Commands) {
+    let layout = HexLayout {
+        orientation: HexOrientation::pointy(),
+        hex_size: HEX_SIZE,
+        ..default()
+    };
+
+    let tile_assets = load_tile_assets(&asset_server);
+
+    let all_hexes: Vec<Hex> = shapes::hexagon(Hex::ZERO, MAP_RADIUS).collect();
+    let altitude_map = generate_altitude_map(&all_hexes);
+
+    // Spawn tiles
+    let entities = all_hexes
+        .into_iter()
+        .map(|hex| {
+            // Random generation
+            let rand_num = rand::random::<u8>() % 4;
+
+            // Hex position and altitude
+            let pos = layout.hex_to_world_pos(hex);
+            let mut altitude = altitude_map.get(&hex).unwrap().clone();
+
+            // let scene_handle;
+            // TODO: loading as scenes break the pickable plugin
+            // if altitude <= SEA_LEVEL {
+            //     scene_handle = tile_assets.ocean.clone();
+            //     altitude = SEA_LEVEL;
+            // } else if altitude >= ELEVATION * 0.8 {
+            //     scene_handle = tile_assets.mountain.clone();
+            // } else if altitude >= ELEVATION * 0.6 {
+            //     scene_handle = tile_assets.hills.clone();
+            // } else {
+            //     scene_handle = tile_assets.grass.clone();
+            // }
+
+
+            let mesh_handle;
+            let material_handle;
+
+            if altitude <= SEA_LEVEL {
+                mesh_handle = tile_assets.ocean_mesh.clone();
+                material_handle = tile_assets.ocean_material.clone();
+                altitude = SEA_LEVEL;
+            } else if altitude >= ELEVATION * 0.8 {
+                mesh_handle = tile_assets.mountain_mesh.clone();
+                material_handle = tile_assets.mountain_material.clone();
+            } else if altitude >= ELEVATION * 0.6 {
+                mesh_handle = tile_assets.hills_mesh.clone();
+                material_handle = tile_assets.hills_material.clone();
+            } else {
+                mesh_handle = tile_assets.grass_mesh.clone();
+                material_handle = tile_assets.grass_material.clone();
+            }
+
+            let id = commands
+                .spawn((
+                    PbrBundle {
+                        transform: Transform::from_xyz(pos.x, altitude, pos.y)
+                            .with_scale(Vec3::splat(2.0)),
+                        mesh: mesh_handle,
+                        material: material_handle,
+                        ..default()
+                    },
+                    // SceneBundle {
+                    //     transform: Transform::from_xyz(pos.x, altitude, pos.y)
+                    //         .with_scale(Vec3::splat(2.0)),
+                    //     scene: scene_handle.clone(),
+                    //     ..Default::default()
+                    // },
+                    PickableBundle::default(), // <- Makes the mesh pickable.
+                    RaycastPickTarget::default(), // <- Needed for the raycast backend.
+                ))
+                .id();
+
+            (hex, id)
+        })
+        .collect();
+
+    commands.insert_resource(Map { entities });
+}
+
+/////////////////////////////// TileAssets ///////////////////////////////////////////////////
+#[derive(Debug, Clone)]
+pub struct SceneTileAssets {
+    pub desert: Handle<Scene>,
+    pub dirt: Handle<Scene>,
+    pub forest: Handle<Scene>,
+    pub grass: Handle<Scene>,
+    pub hills: Handle<Scene>,
+    pub ice: Handle<Scene>,
+    pub jungle: Handle<Scene>,
+    pub mountain: Handle<Scene>,
+    pub ocean: Handle<Scene>,
+    pub plains: Handle<Scene>,
+    pub rocky: Handle<Scene>,
+    pub swamp: Handle<Scene>,
+    pub waste: Handle<Scene>,
+    pub water: Handle<Scene>,
+}
+pub fn load_tile_assets_scene(asset_server: &Res<AssetServer>) -> SceneTileAssets {
+    SceneTileAssets {
+        desert: asset_server.load("tiles/Desert.gltf#Scene0"),
+        dirt: asset_server.load("tiles/Dirt.gltf#Scene0"),
+        forest: asset_server.load("tiles/Forest.gltf#Scene0"),
+        grass: asset_server.load("tiles/Grass.gltf#Scene0"),
+        hills: asset_server.load("tiles/Hills.gltf#Scene0"),
+        ice: asset_server.load("tiles/Ice.gltf#Scene0"),
+        jungle: asset_server.load("tiles/Jungle.gltf#Scene0"),
+        mountain: asset_server.load("tiles/Mountain.gltf#Scene0"),
+        ocean: asset_server.load("tiles/Ocean.gltf#Scene0"),
+        plains: asset_server.load("tiles/Plains.gltf#Scene0"),
+        rocky: asset_server.load("tiles/Rocky.gltf#Scene0"),
+        swamp: asset_server.load("tiles/Swamp.gltf#Scene0"),
+        waste: asset_server.load("tiles/Waste.gltf#Scene0"),
+        water: asset_server.load("tiles/Water.gltf#Scene0"),
+    }
+}
+
+pub struct TileAssets {
+    pub desert_mesh: Handle<Mesh>,
+    pub desert_material: Handle<StandardMaterial>,
+
+    pub dirt_mesh: Handle<Mesh>,
+    pub dirt_material: Handle<StandardMaterial>,
+
+    pub forest_mesh: Handle<Mesh>,
+    pub forest_material: Handle<StandardMaterial>,
+
+    pub grass_mesh: Handle<Mesh>,
+    pub grass_material: Handle<StandardMaterial>,
+
+    pub hills_mesh: Handle<Mesh>,
+    pub hills_material: Handle<StandardMaterial>,
+
+    pub ice_mesh: Handle<Mesh>,
+    pub ice_material: Handle<StandardMaterial>,
+
+    pub jungle_mesh: Handle<Mesh>,
+    pub jungle_material: Handle<StandardMaterial>,
+
+    pub mountain_mesh: Handle<Mesh>,
+    pub mountain_material: Handle<StandardMaterial>,
+
+    pub ocean_mesh: Handle<Mesh>,
+    pub ocean_material: Handle<StandardMaterial>,
+
+    pub plains_mesh: Handle<Mesh>,
+    pub plains_material: Handle<StandardMaterial>,
+
+    pub rocky_mesh: Handle<Mesh>,
+    pub rocky_material: Handle<StandardMaterial>,
+
+    pub swamp_mesh: Handle<Mesh>,
+    pub swamp_material: Handle<StandardMaterial>,
+
+    pub waste_mesh: Handle<Mesh>,
+    pub waste_material: Handle<StandardMaterial>,
+
+    pub water_mesh: Handle<Mesh>,
+    pub water_material: Handle<StandardMaterial>,
+}
+
+pub fn load_tile_assets(asset_server: &Res<AssetServer>) -> TileAssets {
+    TileAssets {
+        desert_mesh: asset_server.load("tiles/Desert.gltf#Mesh0/Primitive0"),
+        desert_material: asset_server.load("tiles/Desert.gltf#Material0"),
+
+        dirt_mesh: asset_server.load("tiles/Dirt.gltf#Mesh0/Primitive0"),
+        dirt_material: asset_server.load("tiles/Dirt.gltf#Material0"),
+
+        forest_mesh: asset_server.load("tiles/Forest.gltf#Mesh0/Primitive0"),
+        forest_material: asset_server.load("tiles/Forest.gltf#Material0"),
+
+        grass_mesh: asset_server.load("tiles/Grass.gltf#Mesh0/Primitive0"),
+        grass_material: asset_server.load("tiles/Grass.gltf#Material0"),
+
+        hills_mesh: asset_server.load("tiles/Hills.gltf#Mesh0/Primitive0"),
+        hills_material: asset_server.load("tiles/Hills.gltf#Material0"),
+
+        ice_mesh: asset_server.load("tiles/Ice.gltf#Mesh0/Primitive0"),
+        ice_material: asset_server.load("tiles/Ice.gltf#Material0"),
+
+        jungle_mesh: asset_server.load("tiles/Jungle.gltf#Mesh0/Primitive0"),
+        jungle_material: asset_server.load("tiles/Jungle.gltf#Material0"),
+
+        mountain_mesh: asset_server.load("tiles/Mountain.gltf#Mesh0/Primitive0"),
+        mountain_material: asset_server.load("tiles/Mountain.gltf#Material0"),
+
+        ocean_mesh: asset_server.load("tiles/Ocean.gltf#Mesh0/Primitive0"),
+        ocean_material: asset_server.load("tiles/Ocean.gltf#Material0"),
+
+        plains_mesh: asset_server.load("tiles/Plains.gltf#Mesh0/Primitive0"),
+        plains_material: asset_server.load("tiles/Plains.gltf#Material0"),
+
+        rocky_mesh: asset_server.load("tiles/Rocky.gltf#Mesh0/Primitive0"),
+        rocky_material: asset_server.load("tiles/Rocky.gltf#Material0"),
+
+        swamp_mesh: asset_server.load("tiles/Swamp.gltf#Mesh0/Primitive0"),
+        swamp_material: asset_server.load("tiles/Swamp.gltf#Material0"),
+
+        waste_mesh: asset_server.load("tiles/Waste.gltf#Mesh0/Primitive0"),
+        waste_material: asset_server.load("tiles/Waste.gltf#Material0"),
+
+        water_mesh: asset_server.load("tiles/Water.gltf#Mesh0/Primitive0"),
+        water_material: asset_server.load("tiles/Water.gltf#Material0"),
+    }
+}
+
+/////////////////////////////// Altitude ///////////////////////////////////////////////////////
+
 // The base probability is 1 when distance is 0 (at the volcano)
 // The probability decreases linearly as the distance increases, down to 0 at the furthest point
-
 fn increment_height(current_height: &mut f32, distance: u32) {
     let distance_f32 = distance as f32;
     let probability = 1.0 - (distance_f32 / MOUNTAIN_SPREAD as f32);
@@ -101,89 +313,6 @@ fn generate_altitude_map(all_hexes: &Vec<Hex>) -> HashMap<Hex, f32> {
     altitude_map
 }
 
-/// Hex grid setup
-fn setup_grid(
-    asset_server: Res<AssetServer>,
-    mut commands: Commands,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    let layout = HexLayout {
-        orientation: HexOrientation::pointy(),
-        hex_size: HEX_SIZE,
-        ..default()
-    };
-
-    // Load assets
-    let mountain_handle: Handle<Mesh> =
-        asset_server.load("mountain/Mountain.gltf#Mesh0/Primitive0");
-    let waste_handle = asset_server.load("12 Wastes.gltf#Mesh0/Primitive0");
-    let hills_handle = asset_server.load("04_Hills.gltf#Mesh0/Primitive0");
-
-    // TODO: load assets as resource and use scenes
-    // let gltf_handle = asset_server.load("mountain/Mountain.gltf#Scene0");
-
-    let all_hexes: Vec<Hex> = shapes::hexagon(Hex::ZERO, MAP_RADIUS).collect();
-    let altitude_map = generate_altitude_map(&all_hexes);
-
-    // Spawn tiles
-    let entities = all_hexes
-        .into_iter()
-        .map(|hex| {
-            // Random generation
-            let rand_num = rand::random::<u8>() % 4;
-
-            // Hex position
-            let pos = layout.hex_to_world_pos(hex);
-            let mut altitude = altitude_map.get(&hex).unwrap().clone();
-
-            // Setup meshes/material handles
-            let mesh_handle;
-            let material_handle;
-
-            // Use random number to choose what tile to create
-            // TODO: determine tile type from altitude, temperature, humidity
-            if rand_num == 0 {
-                mesh_handle = mountain_handle.clone();
-                material_handle = asset_server.load("mountain/Mountain.gltf#Material0");
-            } else if rand_num == 1 {
-                mesh_handle = waste_handle.clone();
-                material_handle = asset_server.load("waste/Waste.gltf#Material0");
-            } else {
-                mesh_handle = hills_handle.clone();
-                material_handle = materials.add(StandardMaterial {
-                    base_color: Color::DARK_GREEN,
-                    metallic: 0.5,
-                    ..Default::default()
-                })
-            }
-
-            let id = commands
-                .spawn((
-                    PbrBundle {
-                        transform: Transform::from_xyz(pos.x, altitude, pos.y)
-                            .with_scale(Vec3::splat(2.0)),
-                        mesh: mesh_handle.clone(),
-                        material: material_handle.clone(),
-                        ..default()
-                    },
-                    // SceneBundle {
-                    //     scene: gltf_handle.clone(),
-                    //     transform: Transform::from_xyz(pos.x, altitude, pos.y)
-                    //         .with_scale(Vec3::splat(1.71)),
-                    //     ..Default::default()
-                    // },
-                    PickableBundle::default(), // <- Makes the mesh pickable.
-                    RaycastPickTarget::default(), // <- Needed for the raycast backend.
-                ))
-                .id();
-
-            (hex, (id, material_handle.clone()))
-        })
-        .collect();
-
-    commands.insert_resource(Map { entities });
-}
-
 ////////////////////// CAMERA MOVEMENT //////////////////////
 ///
 /// /// 3D Orthogrpahic camera setup
@@ -197,6 +326,8 @@ fn setup_camera(mut commands: Commands) {
         RaycastPickCamera::default(), // <- Enable picking for this camera
         CameraController {
             orbit_mode: true,
+            walk_speed: 50.0,
+            run_speed: 100.0,
             ..default()
         },
     ));
